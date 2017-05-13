7e6b57a55baa076ac4fa4661efbf331ae1c2eab3
Rafael Peria de Sene <rpsene@linux.vnet.ibm.com>
2017-05-13 14:29:20 -0300

    Remove date entry from database insert action

--- powergraph.py
+++ powergraph.py
@@ -29,14 +29,15 @@ STORE = False
 
 def savedb(input):
     """
-    Save the execution results in a nosql db
+    Save the execution results in a nosql db. Each new day is represented by a
+    table.
     """
     db = TinyDB('powerdata'+input[1].replace('/', '')+'.json')
     table = db.table(input[1].replace('/', ''))
     # exec  date    time    watts
     # ['2', '2017/5/12', '23:30:22', '707']
-    # we ignore the execution counter
-    table.insert({'date': input[1], 'time': input[2], 'watts': input[3]})
+    # we ignore the execution counter and date, given date is the table name
+    table.insert({'time': input[2], 'watts': input[3]})
     db.close()
 
 
