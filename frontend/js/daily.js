4c783cc4ea1040f2ffc55f3cc21bcb17fc88a7d9
rafaelgpimenta <rafaelgpimenta@gmail.com>
2017-07-11 17:20:55 -0300

    daily reporting functionality

---
+++ frontend/js/daily.js
@@ -0,0 +1,67 @@
+const DATA_FOLDER = "data/test.csv";
+
+// Set the dimensions of the canvas / graph
+var	margin = {top: 30, right: 20, bottom: 30, left: 50},
+	width = 600 - margin.left - margin.right,
+	height = 270 - margin.top - margin.bottom;
+
+// Parse the date / time
+var	parseDate = function(d){return moment.utc(d,"YYYY-MM-DD, HH:mm:ss").toDate();}
+
+// Set the ranges
+var	x = d3.scaleTime().range([0, width]);
+var	y = d3.scaleLinear().range([height, 0]);
+
+// Define the axes
+var	xAxis = d3.axisBottom(x).ticks(5);
+
+var	yAxis = d3.axisLeft(y).ticks(5);
+
+// Define the line
+var	valueline = d3.line()
+	.x(function(d) { return x(d.date); })
+	.y(function(d) { return y(d.close); });
+
+// Adds the svg canvas
+var	svg = d3.select("#scatter")
+	.append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+function getData(form) {
+	var TestVar = form.consumptionDate.value;
+  alert ("You typed: " + TestVar);
+  d3.csv(DATA_FOLDER, function(error, data) {
+  	data.forEach(function(d) {
+  		d.date = parseDate(TestVar + ", " + d.date);
+  		d.close = +d.close;
+  	});
+
+  	// Scale the range of the data
+  	x.domain(d3.extent(data, function(d) { return d.date; }));
+  	y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+  	// Add the valueline path.
+  	svg.append("path")
+  		.attr("class", "line")
+  		.attr("d", valueline(data));
+
+		svg.selectAll(".axis").remove();
+
+
+		// Add the X Axis
+  	svg.append("g")
+  		.attr("class", "x axis")
+  		.attr("transform", "translate(0," + height + ")")
+  		.call(xAxis);
+
+  	// Add the Y Axis
+  	svg.append("g")
+  		.attr("class", "y axis")
+  		.call(yAxis);
+
+  });
+}
